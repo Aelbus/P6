20e091364b18fd8d39068169e47cb53f233913e7
Clement AeLBuS B <becker.clement@gmail.com>
2023-05-16 18:02:54 +0200

    Maj Filtre

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,33 +1,32 @@
 const gallery = document.querySelector(`.gallery`);
 const btnFilter = document.querySelector(`.btn-filter-container`);
 
-//--- Récuparation des projet dans l'API
+//-- Récuparation des projet dans l'API
 async function getWorks() {
   const response = await fetch("http://localhost:5678/api/works");
   const works = await response.json();
   return works;
 }
 
-//--- Récupération des catégories dans l'API
+//-- Récupération des catégories dans l'API
 async function getCategories() {
   const response = await fetch("http://localhost:5678/api/categories");
-  const arrCategories = await response.json();
-//--- Insertion du bouton "Tous"
+  const category = await response.json();
   const btnAll = { id: 0, name: `Tous`};
-  arrCategories.unshift(btnAll);
-  return arrCategories;
+  category.unshift(btnAll);
+  return category;
 }
-//--- Création et affichage des boutons filtres
+//-- Création et affichage des boutons filtres
 async function displayCategoriesBtn() {
-  const arrCategories = await getCategories();
-  for (let i = 0; i < arrCategories.length; i += 1) {
+  const category = await getCategories();
+  for (let i = 0; i < category.length; i += 1) {
     const btn = document.createElement('button');
     btn.classList.add(`btn-filter`);
-    btn.innerText = arrCategories[i].name;
-    btn.setAttribute(`id`, arrCategories[i].id);
+    btn.innerText = category[i].name;
+    btn.setAttribute(`id`, category[i].id);
     btnFilter.appendChild(btn);
 
-    if (arrCategories[i].id === 0) {
+    if (category[i].id === 0) {
       btn.classList.add(`btn-filter:focus`);
     }
     btn.addEventListener(`click`, function() {
@@ -36,13 +35,13 @@ async function displayCategoriesBtn() {
         btn.classList.remove(`btn-filter:focus`);
       });
       btn.classList.toggle(`btn-filter:focus`);
-      showWorksByCategory(arrCategories[i].id);
+      showWorksByCategory(category[i].id);
     });
   }
 }
 displayCategoriesBtn();
 
-// --- Filtrage des projets ---
+//-- Filtrage des projets
 async function showWorksByCategory(categoryId) {
   if (!gallery) {
     return;
